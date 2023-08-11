990960038770b0a9d3611dafaa91e561d5f1bc8e
RFBS18 <fabriziobarrios22@gmail.com>
2023-08-11 09:03:07 -0500

    dibujando un corazon

---
+++ corazon.py
@@ -0,0 +1,15 @@
+from turtle import *
+def curvemode():
+    for i in  range(200):
+        right(1)
+        forward(1)
+color('red', 'pink')
+begin_fill()
+left(140)
+forward(111.65)
+curvemode()
+left(120)
+curvemode()
+forward(111.65)
+end_fill()
+done()
